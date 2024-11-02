808f3d9d261586c88c3031584892e5bdb84e2bb0
clay <rpctropic13@gmail.com>
2024-11-02 18:12:53 -0500

    changed document location

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+"""this script starts a Flask web application that prints Hello HBNB!"""
 from flask import Flask
 app = Flask(__name__)
 
 
-
-"""this script starts a Flask web application that prints Hello HBNB!"""
 @app.route('/', strict_slashes=False)
 def hello():
     return "Hello HBNB!"
