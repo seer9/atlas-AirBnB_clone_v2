8b7de705fa3e6c5c9321b76aa6dd46fed427ab38
clay <rpctropic13@gmail.com>
2024-11-02 18:20:17 -0500

    new route returning HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""this script starts a Flask web application that prints Hello HBNB!"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return "HBNB"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
