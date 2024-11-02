9b9c7797e0b3ea0e5796b774168708ffb07a1c7f
clay <rpctropic13@gmail.com>
2024-11-02 18:33:27 -0500

    app route for passing in text

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""this script starts a Flask web application that prints Hello HBNB!"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_route():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def C_PassedText_route(text):
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
