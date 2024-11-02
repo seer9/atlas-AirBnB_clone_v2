c8f8bc58e712c750862097fa1d7fd3c2a961b6b8
clay <rpctropic13@gmail.com>
2024-11-02 18:15:29 -0500

    black line pep check

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """this script starts a Flask web application that prints Hello HBNB!"""
+
+
 from flask import Flask
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
