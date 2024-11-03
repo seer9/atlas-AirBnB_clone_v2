3f30982c4843ae2d3060307f08e16060bb60f9b7
clay <rpctropic13@gmail.com>
2024-11-02 20:19:46 -0500

    changed reload

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -68,8 +68,7 @@ class DBStorage:
         """Reloads data from the database"""
         Base.metadata.create_all(self.__engine)
         session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
+        self.__session = scoped_session(session_factory)
 
     def close(self):
         """closes the working SQLAlchemy session."""
